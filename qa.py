9152e08e25920389dc0b5af7abd9ccbac9989620
Frederic Lepied <flepied@gmail.com>
2023-08-24 09:44:34 +0200

    Display the document sources at the end of the answer
    - add a new qa.py script to ask a question on the command line

---
+++ qa.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"CLI to ask questions to the agent"
+
+import sys
+
+from dotenv import load_dotenv
+
+import lib
+
+load_dotenv()
+agent = lib.Agent()
+print(agent.question(" ".join(sys.argv[1:])))
+
+# qa.py ends here
